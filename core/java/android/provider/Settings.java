26f623cce5d8278470c9fa4ed57cf5bbe4234e5b
Alberto97 <albertop2197@gmail.com>
2013-12-15 20:51:33 +0100

    Revert "Framework: Add Advanced reboot (2 of 2)"
    This reverts commit 83f8c438c99cac6558b87eb1876e4d8102f9d0a2.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4415,12 +4415,6 @@ public final class Settings {
         public static final String PAYMENT_SERVICE_SEARCH_URI = "payment_service_search_uri";
 
         /**
-         * Whether to include options in power menu for rebooting into recovery or bootloader
-         * @hide
-         */
-        public static final String ADVANCED_REBOOT = "advanced_reboot";
-
-        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -4468,8 +4462,7 @@ public final class Settings {
             MOUNT_UMS_AUTOSTART,
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
-            UI_NIGHT_MODE,
-            ADVANCED_REBOOT
+            UI_NIGHT_MODE
         };
 
         /**
