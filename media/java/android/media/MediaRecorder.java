fd32460c54228e75c1910ab03eb664be571165ca
Alberto97 <albertop2197@gmail.com>
2013-12-18 21:37:34 +0100

    Revert "audio: add support for MSM audio formats"
    This reverts commit 5021c6cf4e792cf0f763fbdd2717459ea93b63bb.
    
    Conflicts:
    	media/java/android/media/MediaRecorder.java
    
    Change-Id: Ia00d70b6723754c027080b486b950461c227511f

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) 2013, The Linux Foundation. All rights reserved.
- * Not a Contribution.
- *
  * Copyright (C) 2007 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -280,12 +277,6 @@ public class MediaRecorder
 
         /** @hide H.264/AAC data encapsulated in MPEG2/TS */
         public static final int OUTPUT_FORMAT_MPEG2TS = 8;
-        /** @hide QCP file format */
-        public static final int QCP = 9;
-        /** @hide 3GPP2 media file format*/
-        public static final int THREE_GPP2 = 10;
-        /** @hide WAVE media file format*/
-        public static final int WAVE = 10;
     };
 
     /**
